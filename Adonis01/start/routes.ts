3aedca9c5faa499bc5ab88a0d6ea89709c144d7e
Afsar Tanvir <afsartanvir805@gmail.com>
2025-11-24 17:02:41 +0600

    Adding new route employee

--- Adonis01/start/routes.ts
+++ Adonis01/start/routes.ts
@@ -15,3 +15,40 @@ router.get('/', async () => {
     name: 'Afsar',
   }
 })
+
+
+
+router.get('/api/employee', async () => {
+  return [
+    {
+      id: 1001,
+      name: 'Afsar Tanvir',
+      date_of_birth: '24-10-2025',
+    },
+    {
+      id: 1002,
+      name: 'Md. Faysal Numin',
+      date_of_birth: '24-10-2023',
+    },
+    {
+      id: 1003,
+      name: 'Mohammad Arman Ahmed Adil',
+      date_of_birth: '24-10-2020',
+    },
+    {
+      id: 1004,
+      name: 'Anamul Hasan',
+      date_of_birth: '24-05-2023',
+    },
+    {
+      id: 1005,
+      name: 'Nahim',
+      date_of_birth: '14-10-2025',
+    },
+    {
+      id: 1006,
+      name: 'kalid Azad',
+      date_of_birth: '24-10-2026',
+    },
+  ]
+})
